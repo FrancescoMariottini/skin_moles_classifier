5ebb23f20124eb594f4cc8aeaa00c465e32f84be
jotwo <jokotek@gmail.com>
2021-01-13 21:42:03 +0100

    new dataset directories names

--- utils/dataset_reorganizing.py
+++ utils/dataset_reorganizing.py
@@ -4,7 +4,6 @@ import shutil
 import pandas as pd
 
 # establishing the main files/directories paths
-
 project_dir_path = os.path.abspath(os.pardir)
 unlabeled_data_dir = 'dataset'
 labeled_data_dir = 'labeled_dataset'
@@ -22,20 +21,20 @@ if os.path.exists(labeled_data_dir_path):
 os.mkdir(labeled_data_dir_path)
 
 # fixing of the subdirectory names based on classes
-danger_dir = 'class_d'
-safety_dir = 'class_s'
+malignant_dir = 'malignant'
+benign_dir = 'benign'
 
 # creation of the subdirectories
-danger_class_path = os.path.join(labeled_data_dir_path, danger_dir)
-safe_class_path = os.path.join(labeled_data_dir_path, safety_dir)
-os.mkdir(danger_class_path)
-os.mkdir(safe_class_path)
+malignant_class_path = os.path.join(labeled_data_dir_path, malignant_dir)
+benign_class_path = os.path.join(labeled_data_dir_path, benign_dir)
+os.mkdir(malignant_class_path)
+os.mkdir(benign_class_path)
 
 # names of original data subdirectories
 unlabeled_data_subdirs = ['SET_D', 'SET_E', 'SET_F']
 
 # copying the pictures from original subdirectories
-# into new ones based on dangerous/safe classes
+# into new ones based on malignant/benign classes
 for subdir in unlabeled_data_subdirs:
     subdir_path = os.path.join(unlabeled_data_dir_path, subdir)
     image_names = os.listdir(subdir_path)
@@ -44,12 +43,29 @@ for subdir in unlabeled_data_subdirs:
             image_prefix = image_name[:-4].lower()
             if(labels_df.loc[image_prefix, 'kat.Diagnose']=='2'
                or labels_df.loc[image_prefix, 'kat.Diagnose']=='3'):
-                image_copy_name = 'd_' + image_prefix + '.bmp'
+                image_copy_name = 'm_' + image_prefix + '.bmp'
                 image_path = os.path.join(subdir_path, image_name)
-                image_copy_path = os.path.join(danger_class_path, image_copy_name)
+                image_copy_path = os.path.join(malignant_class_path, image_copy_name)
                 shutil.copy2(image_path, image_copy_path)
             elif(labels_df.loc[image_prefix, 'kat.Diagnose']=='1'):
-                image_copy_name = 's_' + image_prefix + '.bmp'
+                image_copy_name = 'b_' + image_prefix + '.bmp'
                 image_path = os.path.join(subdir_path, image_name)
-                image_copy_path = os.path.join(safe_class_path, image_copy_name)
+                image_copy_path = os.path.join(benign_class_path, image_copy_name)
                 shutil.copy2(image_path, image_copy_path)
+                
+  
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+                
+                
+                
